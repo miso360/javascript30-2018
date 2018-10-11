83ae4dd922b62a54358ee1438eb74da853c9de65
Jun Park <solsol@Junui-MacBook-Pro.local>
2018-10-12 07:44:53 +0900

    스터디 준비
    스터디 준비

---
+++ 19 - Webcam Fun/README.md
@@ -0,0 +1,2 @@
+webcam 유래?
+index.html에서는 실행이 안된다.
---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,36 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+
+function getVidio(){
+    navigator.mediaDevices.getUserMedia({video:true, audio:false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.log('### error!!!', err);
+        });
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 10);
+}
+
+function takePhoto(){
+    snap.currentTime = 0;
+}
+
+getVidio();
